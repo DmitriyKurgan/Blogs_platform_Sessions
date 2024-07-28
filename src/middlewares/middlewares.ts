c51c0023d62b00a427e9c6a5c49be6b212f0a8a9
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-07-28 13:29:29 +0300

    Fixed tests

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -404,7 +404,7 @@ export const validationRefreshToken = async (
     next: NextFunction
 ) => {
     const refreshToken = req?.cookies?.refreshToken;
-    console.log(refreshToken)
+
     if (!refreshToken) {
         res.sendStatus(401);
         return;
@@ -415,10 +415,35 @@ export const validationRefreshToken = async (
     );
 
     if (!findTokenInBlackList) {
-      return  next();
+        const cookieRefreshTokenObj = await jwtService.verifyToken(
+            refreshToken
+        );
+
+        if (!cookieRefreshTokenObj) {
+            res.sendStatus(401);
+            return;
+        }
+
+        const deviceId = cookieRefreshTokenObj.deviceId;
+        const cookieRefreshTokenIat = cookieRefreshTokenObj.iat;
+
+        const dbDevice = await devicesService.findDeviceById(deviceId);
+
+        if (dbDevice) {
+            if (cookieRefreshTokenIat < dbDevice.lastActiveDate) {
+                res.sendStatus(401);
+                return;
+            }
+        } else {
+            res.sendStatus(401);
+            return;
+        }
+
+        next();
     } else {
       return   res.sendStatus(401);
     }
+
 }
 export const rateLimitMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const ip = req.ip ?? '';
